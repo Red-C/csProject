f521da736314166fa9ba8d114cfb89f55c474d42
Hongfei <hong25100@hotmail.com>
2015-07-23 20:41:27 -0700

    ss

---
+++ command-internals.h
@@ -0,0 +1,46 @@
+// UCLA CS 111 Lab 1 command internals
+#ifndef COMMAND_INTERNALS_H
+#define COMMAND_INTERNALS_H
+#include "set.h"
+enum command_type
+  {
+    AND_COMMAND,         // A && B
+    SEQUENCE_COMMAND,    // A ; B
+    OR_COMMAND,          // A || B
+    PIPE_COMMAND,        // A | B
+    SIMPLE_COMMAND,      // a simple command
+    SUBSHELL_COMMAND    // ( A )
+  };
+
+// Data associated with a command.
+struct command
+{
+  enum command_type type;
+
+  // Exit status, or -1 if not known (e.g., because it has not exited yet).
+  int status;
+
+  // I/O redirections, or 0 if none.
+  char *input;
+  char *output;
+
+  union
+  {
+    // for AND_COMMAND, SEQUENCE_COMMAND, OR_COMMAND, PIPE_COMMAND:
+    struct command *command[2];
+
+    // for SIMPLE_COMMAND:
+    char **word;
+
+    // for SUBSHELL_COMMAND:
+    struct command *subshell_command;
+  } u;
+
+  int* in;
+  int n_in;
+  int* out;
+  int n_out;
+
+};
+
+#endif
